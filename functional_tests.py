00074b209d2a0b054256dc087be9c2eadddd480d
Michael Pohily <mpohily@gmail.com>
2019-05-30 14:36:16 +0000

    Our site now can handle one list item

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         self.browser.quit()
+        
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes 
@@ -39,9 +40,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly', 
+            [row.text for row in rows]
         )
 
         # There is still a text box inviting her to add another item. She
