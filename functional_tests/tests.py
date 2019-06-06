6257c71530688b65748cbc3306d72c4578cabca7
Michael Pohily <mpohily@gmail.com>
2019-06-06 13:37:42 +0000

    move sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -36,13 +36,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
         # Edith goes to check out its homepage
         self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1440, 900)
+        self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
-            720,
+            512,
             delta=10
         )
 
